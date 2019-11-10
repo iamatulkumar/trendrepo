4287776a6bffbb1afdef04a4fed26c838a63731a
Atul Kumar <atul@pocketpills.com>
2019-11-11 00:39:03 +0530

    feat: add theme colors and change primary and dark color

--- app/src/main/java/com/github/trendrepo/ui/MainActivity.kt
+++ app/src/main/java/com/github/trendrepo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.trendrepo
+package com.github.trendrepo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.trendrepo.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/github/trendrepo/viewmodel/PostListViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.trendrepo.viewmodel
+
+import com.github.trendrepo.base.BaseViewModel
+
+class PostListViewModel : BaseViewModel() {
+
+}
