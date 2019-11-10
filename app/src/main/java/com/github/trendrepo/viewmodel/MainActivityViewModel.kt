f323d7860b11a98898ac2c985b38bbfeb53293c7
Atul Kumar <atul@pocketpills.com>
2019-11-11 01:00:45 +0530

    refactor: main activity viewmodel name, add custom toolbar to main activity

--- app/src/main/java/com/github/trendrepo/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/github/trendrepo/viewmodel/MainActivityViewModel.kt
@@ -2,6 +2,6 @@ package com.github.trendrepo.viewmodel
 
 import com.github.trendrepo.base.BaseViewModel
 
-class PostListViewModel : BaseViewModel() {
+class MainActivityViewModel : BaseViewModel() {
 
 }
