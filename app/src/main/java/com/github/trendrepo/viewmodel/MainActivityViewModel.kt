b8ff3fd2a41385455003f14fb307d51251c737c0
Atul Kumar <atul@pocketpills.com>
2019-11-11 01:27:20 +0530

    feat: get repository from api in viewmodel and dispose viewmodel on cleared

--- app/src/main/java/com/github/trendrepo/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/github/trendrepo/viewmodel/MainActivityViewModel.kt
@@ -2,7 +2,12 @@ package com.github.trendrepo.viewmodel
 
 import com.github.trendrepo.base.BaseViewModel
 import com.github.trendrepo.network.RepositoryApi
+import com.github.trendrepo.room.Repository
 import com.github.trendrepo.room.RepositoryDao
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MainActivityViewModel(private val repositoryDao: RepositoryDao) : BaseViewModel() {
@@ -10,7 +15,48 @@ class MainActivityViewModel(private val repositoryDao: RepositoryDao) : BaseView
     @Inject
     lateinit var repositoryApi: RepositoryApi
 
+    private lateinit var subscription: Disposable
+
     init {
+        loadRepositories()
+    }
+
+    private fun loadRepositories() {
+        subscription = Observable.fromCallable { repositoryDao.all }
+            .concatMap { dbRepositoryList ->
+                repositoryApi.getRepositories().concatMap { apiRepositoryList ->
+                    repositoryDao.insertAll(*apiRepositoryList.toTypedArray())
+                    Observable.just(apiRepositoryList)
+                }
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart() }
+            .doOnTerminate { onRetrievePostListFinish() }
+            .subscribe(
+                { result -> onRetrievePostListSuccess(result) },
+                { onRetrievePostListError() }
+            )
+    }
+
+    private fun onRetrievePostListStart() {
 
     }
+
+    private fun onRetrievePostListFinish() {
+
+    }
+
+    private fun onRetrievePostListSuccess(postList: List<Repository>) {
+
+    }
+
+    private fun onRetrievePostListError() {
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
+
 }
