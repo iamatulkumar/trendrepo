4f02ce9d8dc4fef9ff38e695e01f060c411774ea
Atul Kumar <atul@pocketpills.com>
2019-11-11 01:19:06 +0530

    fix: add schema location for room, resolve multidex error

--- app/src/main/java/com/github/trendrepo/base/BaseViewModel.kt
+++ app/src/main/java/com/github/trendrepo/base/BaseViewModel.kt
@@ -1,6 +1,27 @@
 package com.github.trendrepo.base
 
 import androidx.lifecycle.ViewModel
-abstract class BaseViewModel : ViewModel() {
+import com.github.trendrepo.injection.component.DaggerViewModelInjector
+import com.github.trendrepo.injection.component.ViewModelInjector
+import com.github.trendrepo.injection.module.NetworkModule
+import com.github.trendrepo.viewmodel.MainActivityViewModel
 
+abstract class BaseViewModel : ViewModel() {
+    private val injector: ViewModelInjector = DaggerViewModelInjector
+        .builder()
+        .networkModule(NetworkModule)
+        .build()
+
+    init {
+        inject()
+    }
+
+    /**
+     * Injects the required dependencies
+     */
+    private fun inject() {
+        when (this) {
+            is MainActivityViewModel -> injector.inject(this)
+        }
+    }
 }
